6ea4f60cf78e902c9258d52357720720d3320b5c
CyberKL <lotfyk29@yahoo.com>
2025-07-19 21:55:50 +0300

    Change description in homepage

--- src/App.js
+++ src/App.js
@@ -6,14 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <a
-          className="App-link"
-          href="https://example.com"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn Jenkins on Udemy
-        </a>
+        <p>Jenkins Demo App</p>
       </header>
       <p>
           Application version: 1
